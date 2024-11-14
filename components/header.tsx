63999a9e773f782ae0f459247969e36a2cf7a2f4
Maxim Murphy <maxim.murphy8@gmail.com>
2024-11-14 14:16:57 -0500

    Updated Header styling

--- components/header.tsx
+++ components/header.tsx
@@ -12,42 +12,28 @@ const Header = () => {
     setMenuOpen(!menuOpen);
   };
 
-  const activeLinkStyles =
-    "underline underline-offset-4 decoration-allium-lightgreen";
+  const activeLinkStyles = "text-allium-lightgreen font-bold";
 
   const activeDesktopLinkStyles = "text-allium-lightgreen";
 
   return (
     <>
-      <section className="bg-allium-cream top-0 z-50 sticky w-full mb-8 transition duration-300">
-        <div className="h-20 lg:h-fit flex flex-row w-full text-lg lg:text-2xl items-center justify-between font-semibold tracking-normal lg:tracking-tighter px-12 lg:px-32 py-2">
+      <section className="bg-allium-cream top-0 z-50 sticky w-full mb-8 transition duration-300 border-y lg:border-y-2 border-allium-darkbrown">
+        <div className="h-20 lg:h-fit flex flex-row w-full text-lg lg:text-2xl items-center justify-between font-medium lg px-12 lg:px-32 py-2">
           <Link href="/">
-            {router.pathname === "/about" ? (
-              <Image
-                src="/branding/10.png"
-                width={75}
-                height={75}
-                alt="Allium"
-                className="h-16 w-16 lg:h-fit"
-              />
-            ) : (
-              <Image
-                src="/branding/10.png"
-                width={75}
-                height={75}
-                alt="Allium"
-                className="h-16 w-16 lg:h-fit"
-              />
-            )}
+            <Image
+              src="/branding/10.png"
+              width={75}
+              height={75}
+              alt="Allium"
+              className="h-16 w-16 lg:h-fit"
+            />
           </Link>
           <button
             onClick={handleMenuToggle}
-            className={clsx({
-              [activeLinkStyles]: menuOpen,
-              "lg:hidden text-allium-darkbrown": true,
-            })}
+            className="lg:hidden p-2 text-3xl font-normal rounded-xl w-16 h-fit"
           >
-            Menu
+            {menuOpen ? <span>-</span> : <span>+</span>}
           </button>
           <div className="hidden lg:flex flex-row items-center justify-center gap-8 lg:gap-12 text-allium-darkbrown">
             <Link
@@ -86,7 +72,7 @@ const Header = () => {
         {menuOpen && (
           <div
             className={clsx(
-              "absolute h-screen w-full overflow-hidden overscroll-contain bg-allium-darkbrown flex flex-col items-start justify-start gap-6 p-12 text-allium-cream text-[3rem] font-semibold tracking-normal",
+              "absolute h-screen w-full overflow-hidden overscroll-contain bg-allium-darkbrown flex flex-col items-start justify-start gap-6 p-12 text-allium-cream text-[3rem] font-medium tracking-tight",
               "animate-fadeIn"
             )}
           >
@@ -94,6 +80,7 @@ const Header = () => {
               href="/"
               className={clsx({
                 [activeLinkStyles]: router.pathname === "/",
+                "w-full pb-2 border-b-2 border-allium-lightbrown": true,
               })}
             >
               HOME
@@ -104,6 +91,7 @@ const Header = () => {
               rel="noopener noreferrer"
               className={clsx({
                 [activeLinkStyles]: router.pathname === "/apparel",
+                "w-full pb-2 border-b-2 border-allium-lightbrown": true,
               })}
             >
               APPAREL
@@ -112,6 +100,7 @@ const Header = () => {
               href="/products"
               className={clsx({
                 [activeLinkStyles]: router.pathname === "/products",
+                "w-full pb-2 border-b-2 border-allium-lightbrown": true,
               })}
             >
               PRODUCTS
@@ -120,6 +109,7 @@ const Header = () => {
               href="/posts"
               className={clsx({
                 [activeLinkStyles]: router.pathname === "/posts",
+                "w-full pb-2 border-b-2 border-allium-lightbrown": true,
               })}
             >
               BLOG
@@ -128,6 +118,7 @@ const Header = () => {
               href="/about"
               className={clsx({
                 [activeLinkStyles]: router.pathname === "/about",
+                "w-full pb-2 border-b-2 border-allium-lightbrown": true,
               })}
             >
               ABOUT
