0c695f9bb24cfb67fcb7416ddc53c1b021acbf4a
Maxim Murphy <maxim.murphy8@gmail.com>
2024-11-20 16:06:51 -0500

    Added footer reveal on scroll

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,27 +1,30 @@
-import Container from "./container";
 import Link from "next/link";
 import Image from "next/image";
 import { Icon } from "@iconify/react";
+import { clsx } from "clsx";
+import { useRouter } from "next/router";
+
 import AlliumFooterLogo from "/public/branding/20.png";
 
 const Footer = () => {
+  const router = useRouter();
   const currentYear = new Date().getFullYear();
 
+  const onPageStyles = "hidden";
+
   return (
-    <footer className="bg-allium-green border-y lg:border-y-2 border-allium-darkbrown text-allium-cream px-4 py-12 lg:py-16">
-      <Container>
-        <div className="flex flex-row items-start justify-between mb-12 gap-4">
-          <Link href="/">
-            <Image
-              src={AlliumFooterLogo}
-              width={2400}
-              height={600}
-              alt="Allium"
-              placeholder="blur"
-              className="h-12 lg:h-16 w-auto"
-            />
-          </Link>
-          <div className="w-fit flex flex-row gap-6 lg:gap-8 font-semibold mt-2 lg:mt-0">
+    <footer
+      className="relative h-[36rem] lg:h-[40rem]"
+      style={{ clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)" }}
+    >
+      <div className="fixed w-full bottom-0 h-[36rem] lg:h-[40rem] bg-allium-green text-allium-cream px-4 lg:px-12 pt-12 lg:pt-16">
+        <div className="w-full lg:w-96 text-base lg:text-lg flex flex-row items-start justify-between mb-24 lg:mb-36 px-8 text-left py-6 lg:py-0 border-y lg:border-none border-allium-darkgreen">
+          <div className="flex flex-col gap-4">
+            <Link href="/" className="w-full">
+              Home
+            </Link>
+            <Link href="/about">About</Link>
+            <Link href="/contact">Contact</Link>
             <Link
               href="https://www.etsy.com/shop/AlliumApparel"
               target="_blank"
@@ -33,6 +36,11 @@ const Footer = () => {
                 className="w-6 h-6 lg:w-8 lg:h-8"
               />
             </Link>
+          </div>
+          <div className="flex flex-col gap-4">
+            <Link href="/apparel">Apparel</Link>
+            <Link href="/products">Products</Link>
+            <Link href="/blog">Blog</Link>
             <Link
               href="https://www.pinterest.com/alliumlover/"
               target="_blank"
@@ -46,30 +54,35 @@ const Footer = () => {
             </Link>
           </div>
         </div>
-        <div className="relative flex">
-          <div className="w-full lg:w-64 text-lg flex flex-row items-start justify-between mb-12 py-4 border-y lg:border-none border-allium-darkgreen">
-            <div className="flex flex-col gap-4">
-              <Link href="/about">About</Link>
-              <Link href="/blog">Blog</Link>
-              <Link href="/contact">Contact</Link>
-            </div>
-            <div className="flex flex-col gap-4">
-              <Link href="/apparel">Apparel</Link>
-              <Link href="/products">Products</Link>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col items-start justify-center gap-4 text-justify text-allium-cream/60">
-          <p className="w-fit text-xs">
+
+        <div className="w-full flex flex-col lg:flex-row items-center lg:items-end justify-between gap-4 mb-8">
+          <Link href="/">
+            <Image
+              src={AlliumFooterLogo}
+              width={2400}
+              height={600}
+              alt="Allium"
+              placeholder="blur"
+              className="h-20 lg:h-48 w-auto"
+            />
+          </Link>
+          <p className="w-fit text-sm text-allium-cream/80">
             &copy; {currentYear} AlliumApparel. All rights reserved.
           </p>
-          <p className="w-fit text-xs">
+        </div>
+
+        <div className="w-full  flex items-end justify-center text-center text-allium-cream/40 text-xs">
+          <p
+            className={clsx({
+              [onPageStyles]: router.pathname === "/",
+            })}
+          >
             As an Amazon Associate I earn from qualifying purchases. Allium
             receives a commission on all purchases made through our Amazon
             links.
           </p>
         </div>
-      </Container>
+      </div>
     </footer>
   );
 };
--- components/marquee.tsx
+++ components/marquee.tsx
@@ -1,6 +1,6 @@
 export default function Marquee() {
   return (
-    <div className="w-full flex overflow-hidden font-semibold text-base lg:text-xl py-4 border-t lg:border-t-2 border-allium-darkbrown">
+    <div className="w-full flex overflow-hidden font-semibold text-base lg:text-xl py-4 border-y lg:border-y-2 border-allium-darkbrown">
       <div className="w-max flex animate-marquee">
         <p className="w-36 lg:w-64">I 🤎 GARLIC</p>
         <p className="w-36 lg:w-64">I 💚 ONIONS</p>
